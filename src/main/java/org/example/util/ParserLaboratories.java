e6c7743d98eafe37bd35e23ff272d91ade757b16
morrss <grishkovea@gmail.com>
2024-02-21 16:43:43 +0300

    начал делать парсер лабораторий

--- src/main/java/org/example/util/ParserLaboratories.java
+++ src/main/java/org/example/util/ParserLaboratories.java
@@ -1,5 +1,20 @@
 package org.example.util;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 public class ParserLaboratories {
+    public static TreeMap<Class<?>, String> parserLaboratories(){
+        String path = System.getProperty("user.dir") + "/src/main/java/Programming/FourthSemLab";
+        var classes = new ArrayList<Class<?>>();
+        var dirWhereLabs = new File(path);
+        var files = dirWhereLabs.listFiles();
+        assert files != null;
+        for(var file:files){
+
+        }
+        return null;
+    }
 
 }
