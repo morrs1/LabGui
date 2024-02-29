1adafeecbeec48125b12a8a3533ec714188d4f2d
morrs1 <grishkovea@gmail.com>
2024-02-29 22:21:49 +0300

    сделал 4 задание 3 лабы по джаве

--- src/main/java/Programming/FourthSemLab/ThirdLab/Circles.java
+++ src/main/java/Programming/FourthSemLab/ThirdLab/Circles.java
@@ -2,15 +2,13 @@ package Programming.FourthSemLab.ThirdLab;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
+
 
 public class Circles {
 
   static int defineCircles(String args) {
     var arr = new ArrayList<Double>();
-    Arrays.stream(args.split(" ")).forEach(x -> {
-      arr.add(Double.parseDouble(x));
-    });
+    Arrays.stream(args.split(" ")).forEach(x -> arr.add(Double.parseDouble(x)));
 
     var distance = Math.sqrt(
         Math.pow(arr.get(0) - arr.get(3), 2) + Math.pow(arr.get(1) - arr.get(4), 2));
@@ -21,7 +19,8 @@ public class Circles {
     if (distance == arr.get(2) + arr.get(5)) {
       return 2;
     }
-    if (distance < arr.get(2) + arr.get(5) && (distance > Math.max(arr.get(2), arr.get(5)) - Math.min(arr.get(2), arr.get(5)))) {
+    if (distance < arr.get(2) + arr.get(5) && (distance
+        > Math.max(arr.get(2), arr.get(5)) - Math.min(arr.get(2), arr.get(5)))) {
       return 3;
     }
     if (distance + arr.get(2) <= arr.get(5)) {
