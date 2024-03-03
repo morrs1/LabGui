1a79c94725e5544e68625f397f2013dbb29ad885
morrss <grishkovea@gmail.com>
2024-03-03 17:26:11 +0300

    сделал 1 задание 4 лабы по жаве

--- src/main/java/Programming/FourthSemLab/FourthLab/car/Car.java
+++ src/main/java/Programming/FourthSemLab/FourthLab/car/Car.java
@@ -1,13 +1,16 @@
 package Programming.FourthSemLab.FourthLab.car;
 
+
 public class Car {
     private String regPlate;
-    private String brand;
-    private CarType type;
+    private final String brand;
+    private final CarType type;
     private String color;
     private Integer enginePower;
-    private Integer amountOfWheels;
+    private final Integer amountOfWheels;
+
     public enum CarType {PASSENGER, TRUCK, BUS}
+
     public Car(String brand, CarType type, String color, Integer enginePower, Integer amountOfWheels) {
         this.brand = brand;
         this.type = type;
@@ -17,7 +20,9 @@ public class Car {
     }
 
     public Car(String regPlate, String brand, CarType type, String color, Integer enginePower, Integer amountOfWheels) {
-        this.regPlate = regPlate;
+        if (checkRegPlate(regPlate)) this.regPlate = regPlate;
+        else
+            System.out.println("Неправильно введен номер");
         this.brand = brand;
         this.type = type;
         this.color = color;
@@ -26,7 +31,9 @@ public class Car {
     }
 
     public void setRegPlate(String regPlate) {
-        this.regPlate = regPlate;
+        if (checkRegPlate(regPlate)) this.regPlate = regPlate;
+        else
+            System.out.println("Неправильно введен номер");
     }
 
     public void setColor(String color) {
@@ -47,5 +54,34 @@ public class Car {
                 ", enginePower=" + enginePower +
                 ", amountOfWheels=" + amountOfWheels +
                 '}';
+    }
+
+    private boolean checkRegPlate(String regPlate) {
+        var regex = "[АВЕКМНОРСТУХ]\\d{3}[АВЕКМНОРСТУХ]{2}\\d{2,3}RUS";
+        return regPlate.matches(regex);
+    }
+
+    public String getRegPlate() {
+        return regPlate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public CarType getType() {
+        return type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Integer getEnginePower() {
+        return enginePower;
+    }
+
+    public Integer getAmountOfWheels() {
+        return amountOfWheels;
     }
 }
