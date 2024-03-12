501b35654de32e3a368edb302c7c2458e990b78a
morrs1 <grishkovea@gmail.com>
2024-03-12 22:34:38 +0300

    начал делать CarBase

--- src/main/java/Programming/FourthSemLab/FourthLab/cars/CarBase.java
+++ src/main/java/Programming/FourthSemLab/FourthLab/cars/CarBase.java
@@ -3,8 +3,14 @@ package Programming.FourthSemLab.FourthLab.cars;
 import java.util.ArrayList;
 
 public class CarBase {
-    ArrayList<Car> carsParked = new ArrayList<>();
+    ArrayList<Car> carsParked;
     ArrayList<Car> carsInTransit;
     ArrayList<Car> carsInRepair;
+
+    public CarBase(Integer maxAmountOfCars){
+        carsParked = new ArrayList<>(maxAmountOfCars);
+        carsInRepair = new ArrayList<>(maxAmountOfCars);
+        carsInTransit = new ArrayList<>(maxAmountOfCars);
+    }
 
 }
