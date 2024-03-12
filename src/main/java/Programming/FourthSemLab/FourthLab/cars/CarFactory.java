edafe1058dc71dc4973b3a6cf9b73eb7630e7e4f
morrs1 <grishkovea@gmail.com>
2024-03-12 22:21:11 +0300

    доделал CarFactory

--- src/main/java/Programming/FourthSemLab/FourthLab/cars/CarFactory.java
+++ src/main/java/Programming/FourthSemLab/FourthLab/cars/CarFactory.java
@@ -1,8 +1,18 @@
 package Programming.FourthSemLab.FourthLab.cars;
 
+import static Programming.FourthSemLab.FourthLab.cars.Car.CarType.PASSENGER;
+import static Programming.FourthSemLab.FourthLab.cars.Car.CarType.TRUCK;
+
+import Programming.FourthSemLab.FourthLab.cars.Car.CarType;
+
 public class CarFactory {
 
-  public Car createCar(){
-
+  public static Car createCar(CarType type, String brand, String color, Engine engine,
+      Integer numOfWheels) {
+    return switch (type) {
+      case BUS -> new Bus(brand, color, engine, numOfWheels);
+      case TRUCK -> new Truck(brand, color, engine, numOfWheels);
+      case PASSENGER -> new Passenger(brand, color, engine, numOfWheels);
+    };
   }
 }
