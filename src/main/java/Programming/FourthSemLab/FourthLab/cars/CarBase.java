62d07771405b2cb94f1c8150a2baafd892874418
morrs1 <grishkovea@gmail.com>
2024-03-14 20:52:45 +0300

    разбил логику в 7 задании

--- src/main/java/Programming/FourthSemLab/FourthLab/cars/CarBase.java
+++ src/main/java/Programming/FourthSemLab/FourthLab/cars/CarBase.java
@@ -14,27 +14,15 @@ public class CarBase {
     this.maxAmountOfCars = maxAmountOfCars;
   }
 
-  public void addCarToBase(Car car) {
-    if (carsParked.size() < maxAmountOfCars) {
-      carsParked.add(car);
-    } else {
-      throw new IllegalStateException("The array is full");
-    }
-  }
 
+
+  public void addCarToBase(Car car){
+    addCarToArray(car, carsParked);
+  }
   public void removeCarFromBase(Car car) {
     removeCarFromArray(car, carsParked);
   }
 
-  public void removeCarFromArray(Car car, ArrayList<Car> arr) {
-    for (int i = 0; i < arr.size(); i++) {
-      if (arr.get(i) == car) {
-        arr.remove(i);
-        return;
-      }
-    }
-    throw new IllegalArgumentException("The car is not in the array");
-  }
 
   @Override
   public String toString() {
@@ -43,5 +31,21 @@ public class CarBase {
         "\n carsInTransit=" + carsInTransit +
         "\n carsInRepair=" + carsInRepair +
         '}';
+  }
+
+  private void addCarToArray(Car car, ArrayList<Car> arr) {
+    if (arr.size() < maxAmountOfCars) {
+      arr.add(car);
+    } else {
+      throw new IllegalStateException("The array is full");
+    }
+  }
+
+  private void removeCarFromArray(Car car, ArrayList<Car> arr) {
+    if (!arr.contains(car)) {
+      throw new IllegalArgumentException("The car is not in the array");
+    }
+    arr.remove(car);
+
   }
 }
