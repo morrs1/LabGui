1f4a208cb5772ca0f857923ce319c20dad9b3e2b
morrs1 <grishkovea@gmail.com>
2024-03-23 21:56:15 +0300

    ..

--- src/main/java/Formal_languages/fifthLab/Main.java
+++ src/main/java/Formal_languages/fifthLab/Main.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 import org.checkerframework.checker.units.qual.A;
@@ -22,29 +24,109 @@ public class Main extends Formal_languages.firstLab.Main {
 
   static Scanner scanner = new Scanner(System.in);
   private static final Graph<String, NamedEdge> graph = new DefaultDirectedGraph<>(NamedEdge.class);
-  private static ArrayList<Character> alphabet;
+  private static ArrayList<Character> alphabet = new ArrayList<>(Arrays.asList('a', 'b', 'c'));
   private static ArrayList<String> arrConditions;
 
   public static void main(String[] args) {
-    alphabet = setupAlphabet();
-    System.out.println("Введите кол-во вершин: ");
-    arrConditions = setupConditions(scanner.nextInt());
-    var arrStart = setupVertex(arrConditions, "начальные");
-    var arrEnd = setupVertex(arrConditions, "конечные");
+//    alphabet = setupAlphabet();
+//    System.out.println("Введите кол-во вершин: ");
+//    arrConditions = setupConditions(scanner.nextInt());
+//    var arrStart = setupVertex(arrConditions, "начальные");
+//    var arrEnd = setupVertex(arrConditions, "конечные");
+//
+//    // Добавление вершин
+//    addVertexToGraph(arrConditions);
+//    var mainGraph = addEdgesToGraph();
+//    System.out.println(mainGraph);
+//
+//    // Вывод информации о графе
+//    System.out.println("Вершины графа:\n" + graph.vertexSet());
+//    System.out.println("Граф:\n" + String.join("\n",
+//        graph.edgeSet().toString().replace("]", "").replace("[", "").split(" , ")));
+//    System.out.println(
+//        "Начальные:\n" + arrStart + "\nКонечные:\n" + arrEnd + "\nАлфавит: " + alphabet);
+//    var epsilonClosure = calculateEpsilonClosure(mainGraph);
+//    System.out.println("Эпсилон-замыкания: " + epsilonClosure);
+//    System.out.println(calculateSTable(mainGraph, epsilonClosure));
 
-    // Добавление вершин
-    addVertexToGraph(arrConditions);
-    var rr = addEdgesToGraph();
-    System.out.println(rr);
+    Map<String, Set<String>> epsilonClosure = new HashMap<>();
+    Set<String> v0Set = new HashSet<>();
+    v0Set.add("v0");
+    v0Set.add("v3");
+    epsilonClosure.put("v0", v0Set);
+    Set<String> v1Set = new HashSet<>();
+    v1Set.add("v1");
+    v1Set.add("v2");
+    epsilonClosure.put("v1", v1Set);
+    Set<String> v2Set = new HashSet<>();
+    v2Set.add("v2");
+    epsilonClosure.put("v2", v2Set);
+    Set<String> v3Set = new HashSet<>();
+    v3Set.add("v3");
+    epsilonClosure.put("v3", v3Set);
 
-    // Вывод информации о графе
-    System.out.println("Вершины графа:\n" + graph.vertexSet());
-    System.out.println("Граф:\n" + String.join("\n",
-        graph.edgeSet().toString().replace("]", "").replace("[", "").split(" , ")));
-    System.out.println(
-        "Начальные:\n" + arrStart + "\nКонечные:\n" + arrEnd + "\nАлфавит: " + alphabet);
-    System.out.println("Эпсилон-замыкания: " + calculateEpsilonClosure(rr));
+    Map<String, Map<String, Set<String>>> mainTable = new HashMap<>();
+    Map<String, Set<String>> v0Map = new HashMap<>();
+    v0Map.put("a", new HashSet<>(Set.of("v1")));
+    v0Map.put("b", new HashSet<>(Set.of("v0")));
+    v0Map.put("ε", new HashSet<>(Set.of("v3")));
+    mainTable.put("v0", v0Map);
+    Map<String, Set<String>> v1Map = new HashMap<>();
+    v1Map.put("c", new HashSet<>(Set.of("v3")));
+    v1Map.put("ε", new HashSet<>(Set.of("v2")));
+    mainTable.put("v1", v1Map);
+    Map<String, Set<String>> v2Map = new HashMap<>();
+    v2Map.put("a", new HashSet<>(Set.of("v3")));
+    v2Map.put("b", new HashSet<>(Set.of("v1")));
+    mainTable.put("v2", v2Map);
+    Map<String, Set<String>> v3Map = new HashMap<>();
+    v3Map.put("c", new HashSet<>(Set.of("v3")));
+    mainTable.put("v3", v3Map);
 
+    System.out.println(mainTable);
+    System.out.println(epsilonClosure);
+
+    Map<String, Map<String, Set<String>>> bagaTransitions = new HashMap<>();
+
+// Transitions for v0
+    Map<String, Set<String>> v0Transitions = new HashMap<>();
+    v0Transitions.put("a", Set.of("v1"));
+    v0Transitions.put("b", Set.of("v0"));
+    v0Transitions.put("ε", Set.of("v3"));
+    bagaTransitions.put("v0", v0Transitions);
+
+// Transitions for v1
+    Map<String, Set<String>> v1Transitions = new HashMap<>();
+    v1Transitions.put("c", Set.of("v3"));
+    v1Transitions.put("ε", Set.of("v2"));
+    bagaTransitions.put("v1", v1Transitions);
+
+// Transitions for v2
+    Map<String, Set<String>> v2Transitions = new HashMap<>();
+    v2Transitions.put("a", Set.of("v3"));
+    v2Transitions.put("b", Set.of("v1"));
+    bagaTransitions.put("v2", v2Transitions);
+
+// Transitions for v3
+    Map<String, Set<String>> v3Transitions = new HashMap<>();
+    v3Transitions.put("c", Set.of("v3"));
+    bagaTransitions.put("v3", v3Transitions);
+
+
+
+    var calcSTable = calculateSTable(mainTable, epsilonClosure);
+    System.out.println(calcSTable);
+    var bufMap = new HashMap<Set<String>, String>();
+    var cc= 0;
+    for (var cs : calcSTable.keySet()) {
+      bufMap.put(cs, String.format("S%d", cc));
+      cc+=1;
+    }
+
+
+    System.out.println("\n" + bufMap);
+    transformDictionary(calcSTable, bufMap);
+    System.out.println(calcSTable);
   }
 
   private static ArrayList<String> setupConditions(Integer amount) {
@@ -67,7 +149,9 @@ public class Main extends Formal_languages.firstLab.Main {
         break;
       }
 
-      if(str.isEmpty()) continue;
+      if (str.isEmpty()) {
+        continue;
+      }
       if (!arrV.contains(str)) {
         System.out.println("Такой вершины нет во множестве вершин, введите заново: ");
       } else if (!arrayV.contains(str)) {
@@ -110,27 +194,16 @@ public class Main extends Formal_languages.firstLab.Main {
 
   private static Map<String, Set<String>> calculateEpsilonClosure(
       Map<String, Map<String, Set<String>>> graph) {
-
-    // Создаем пустую карту для хранения эпсилон-замыканий
     Map<String, Set<String>> closures = new HashMap<>();
 
-    // Проходим по всем узлам графа
+    // Iterate over all vertices in the graph
     for (String vertex : graph.keySet()) {
-
-      // Создаем пустое множество для эпсилон-замыкания текущего узла
       Set<String> closure = new HashSet<>();
-
-      // Добавляем сам узел в его эпсилон-замыкание
       closure.add(vertex);
-
-      // Рекурсивно строим эпсилон-замыкание
       calculateEpsilonClosureHelper(graph, vertex, closure);
-
-      // Сохраняем эпсилон-замыкание для текущего узла
-      closures.put(vertex, closure);
+      closures.put(vertex, closure); // Store closure for each vertex
     }
 
-    // Возвращаем карту с эпсилон-замыканиями
     return closures;
   }
 
@@ -153,6 +226,65 @@ public class Main extends Formal_languages.firstLab.Main {
         }
       }
     }
+  }
+
+
+  private static Map<Set<String>, Map<String, Set<String>>> calculateSTable(
+      Map<String, Map<String, Set<String>>> mainTable, Map<String, Set<String>> epsilonClosure) {
+
+    Map<Set<String>, Map<String, Set<String>>> sTable = new HashMap<>();
+
+    // Итерируем по всем эпсилон-замыканиям
+    for (Set<String> closure : epsilonClosure.values()) {
+      Map<String, Set<String>> transitions = new HashMap<>();
+
+      // Итерируем по всем символам алфавита
+      for (Character character : alphabet) {
+        Set<String> reachableStates = new HashSet<>();
+
+        // Для каждой вершины в замыкании
+        for (String vertex : closure) {
+          // Проверяем, есть ли переход по символу из этой вершины
+          if (mainTable.get(vertex).containsKey(character.toString())) {
+            // Добавляем достижимые вершины в множество
+            reachableStates.addAll(mainTable.get(vertex).get(character.toString()));
+          }
+        }
+
+        // Добавляем эпсилон-замыкания для всех достижимых вершин
+        for (String state : reachableStates) {
+          reachableStates.addAll(epsilonClosure.get(state));
+        }
+
+        // Добавляем переход по символу в таблицу S
+        transitions.put(character.toString(), reachableStates);
+      }
+
+      // Добавляем строку в таблицу S для текущего эпсилон-замыкания
+      sTable.put(closure, transitions);
+    }
+
+    return sTable;
+  }
+
+
+  public static void transformDictionary(
+      Map<Set<String>, Map<String, Set<String>>> dict1,
+      Map<Set<String>, String> dict2) {
+var hs = new HashSet<String>();
+    for (var fL: dict1.keySet()){
+      for(var sL: dict1.get(fL).keySet()){
+        for(var tL: dict2.keySet()){
+          if (dict1.get(fL).get(sL).containsAll(tL)){
+            hs.add(dict2.get(tL));
+          }
+        }
+        dict1.get(fL).get(sL).clear();
+        dict1.get(fL).get(sL).addAll(hs);
+        hs.clear();
+      }
+    }
+
   }
 
 
