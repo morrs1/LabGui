8a4cf2970140d6d2f40a79481d9f4940c9cdffe5
morrs1 <grishkovea@gmail.com>
2024-04-01 19:49:20 +0300

    переделал метод сз угла

--- src/main/java/expOperation/firstLab/Main.java
+++ src/main/java/expOperation/firstLab/Main.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        var mainTable = new TableTransportTask();
-        mainTable.setTable();
-        System.out.println(mainTable);
-    }
+  public static void main(String[] args) {
+    int[][] costs = {
+        {1, 2, 3, 5, 2},
+        {4, 6, 7, 3, 1},
+        {2, 2, 3, 4, 5}
+    };
+    int[] supplies = {120, 120, 200, 180, 110};
+    int[] demands = {200, 150, 350};
+
+    TableTransportTask mainTable = new TableTransportTask(costs, supplies, demands);
+    System.out.println(mainTable);
+    var ss = mainTable.northWestAngle();
+    System.out.println(ss);
+  }
 
 
 }
--- src/main/java/expOperation/firstLab/TableTransportTask.java
+++ src/main/java/expOperation/firstLab/TableTransportTask.java
@@ -1,70 +1,65 @@
 package expOperation.firstLab;
 
-import java.util.LinkedHashMap;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class TableTransportTask {
 
-  private LinkedHashMap<HeadNode, LinkedHashMap<HeadNode, Node>> table;
-  private static final Scanner scanner = new Scanner(System.in);
-
-  public void setTable() {
-    var mainTable = new LinkedHashMap<HeadNode, LinkedHashMap<HeadNode, Node>>();
-    // Создаем внешний LinkedHashMap для первого внешнего ключа
-    LinkedHashMap<HeadNode, Node> innerMap1 = new LinkedHashMap<>();
-    innerMap1.put(new HeadNode("b1", 120), new Node(1));
-    innerMap1.put(new HeadNode("b2",120), new Node(2));
-    innerMap1.put(new HeadNode("b3", 200), new Node(3));
-    innerMap1.put(new HeadNode("b4",180), new Node(5));
-    innerMap1.put(new HeadNode("b5", 110), new Node(2));
-
-    // Создаем внешний LinkedHashMap для второго внешнего ключа
-    LinkedHashMap<HeadNode, Node> innerMap2 = new LinkedHashMap<>();
-    innerMap2.put(new HeadNode("b1", 120), new Node(4));
-    innerMap2.put(new HeadNode("b2",120), new Node(6));
-    innerMap2.put(new HeadNode("b3", 200), new Node(7));
-    innerMap2.put(new HeadNode("b4",180), new Node(3));
-    innerMap2.put(new HeadNode("b5", 110), new Node(1));
-
-    // Создаем внешний LinkedHashMap для третьего внешнего ключа
-    LinkedHashMap<HeadNode, Node> innerMap3 = new LinkedHashMap<>();
-    innerMap3.put(new HeadNode("b1", 120), new Node(2));
-    innerMap3.put(new HeadNode("b2",120), new Node(2));
-    innerMap3.put(new HeadNode("b3", 200), new Node(3));
-    innerMap3.put(new HeadNode("b4",180), new Node(4));
-    innerMap3.put(new HeadNode("b5", 110), new Node(5));
+  private int[][] costs; // Затраты на перевоз товаров
+  private int[] supplies; // Объемы товаров, которые нужно перевезти из каждого пункта отправления
+  private int[] demands; // Объемы товаров, которые нужно принять в каждом пункте назначения
 
 
-    // Добавляем внутренние LinkedHashMap в основную таблицу
-    mainTable.put(new HeadNode("a1", 200), innerMap1);
-    mainTable.put(new HeadNode("a2", 150), innerMap2);
-    mainTable.put(new HeadNode("a3", 350), innerMap3);
-
-    this.table = mainTable;
+  public TableTransportTask(int[][] costs, int[] supplies, int[] demands) {
+    this.costs = costs;
+    this.supplies = supplies;
+    this.demands = demands;
   }
+
+  public TableTransportTask northWestAngle() {
+    int[][] amount = new int[3][5];
+    var i = 0;
+    var j = 0;
+    while (i < costs.length && j < costs[0].length) {
+      if (demands[i] > supplies[j]) {
+        amount[i][j] = supplies[j];
+        demands[i] -= supplies[j];
+        j++;
+      } else if (demands[i] < supplies[j]) {
+        amount[i][j] = demands[i];
+        supplies[j] -= demands[i];
+        i++;
+      } else {
+        amount[i][j] = demands[i];
+        i++;
+        j++;
+      }
+    }
+
+    return new TableTransportTask(amount, this.supplies, this.demands);
+  }
+
+
 
   @Override
   public String toString() {
-    StringBuilder sb = new StringBuilder();
-    // Предполагаем, что максимальная длина строки для каждого столбца равна 10 символам
-    int columnWidth = 10;
-
-    // Выводим заголовки столбцов
-    sb.append(String.format("%-" + columnWidth + "s", "")); // Пустой столбец для заголовка столбцов
-    for (HeadNode innerKey : table.values().iterator().next().keySet()) {
-      sb.append(String.format("%-" + columnWidth + "s", innerKey.toString()));
+    var res = new StringBuilder();
+    var str = new StringBuilder().append("      ");
+    for (var s : supplies) {
+      str.append(s).append("      ");
     }
-    sb.append("\n");
+    res.append(str).append("\n");
 
-    // Выводим строки таблицы
-    for (HeadNode outerKey : table.keySet()) {
-      sb.append(String.format("%-" + columnWidth + "s", outerKey.toString()));
-      for (Node node : table.get(outerKey).values()) {
-        sb.append(String.format("%-" + columnWidth + "s", node.toString()));
+    for (var i = 0; i < costs.length; i++) {
+      str = new StringBuilder();
+      str.append(demands[i]).append("    ");
+      for (var j = 0; j < costs[0].length; j++) {
+        str.append(costs[i][j]).append("        ");
       }
-      sb.append("\n");
+      str.append("\n");
+      res.append(str);
     }
 
-    return sb.toString();
+    return res.toString();
   }
+
 }
