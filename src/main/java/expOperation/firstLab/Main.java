6a7d14d3e715593c27b7c17624e744c08a32dd57
morrs1 <grishkovea@gmail.com>
2024-04-03 17:50:42 +0300

    сделал нормальный вывод в amount без нулей

--- src/main/java/expOperation/firstLab/Main.java
+++ src/main/java/expOperation/firstLab/Main.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    Integer[][] costs = {
+    int[][] costs = {
         {1, 2, 3, 5, 2},
         {4, 6, 7, 3, 1},
         {2, 2, 3, 4, 5}
     };
-    Integer[] supplies = {120, 120, 200, 180, 110};
-    Integer[] demands = {200, 150, 350};
-
+    int[] supplies = {120, 120, 200, 180, 110};
+    int[] demands = {200, 150, 350};
     TableTransportTask mainTable = new TableTransportTask(costs, supplies, demands);
-    System.out.println(mainTable);
+    mainTable.printCosts();
     mainTable.checkForEquality();
-    var ss = mainTable.northWestAngle();
-    System.out.println(mainTable);
-    System.out.println(ss);
+    mainTable.northWestAngle();
+    mainTable.printCosts();
+    mainTable.printAmount();
+
 
   }
 
--- src/main/java/expOperation/firstLab/TableTransportTask.java
+++ src/main/java/expOperation/firstLab/TableTransportTask.java
@@ -5,14 +5,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class TableTransportTask {
 
-    private Integer[][] costs; // Затраты на перевоз товаров
-    private Integer[] demands; // Объемы товаров, которые нужно перевезти из каждого пункта отправления
-    private Integer[] supplies; // Объемы товаров, которые нужно принять в каждом пункте назначения
+    private int[][] costs; // Затраты на перевоз товаров
+    private int[] demands; // Объемы товаров, которые нужно перевезти из каждого пункта отправления
+    private int[] supplies; // Объемы товаров, которые нужно принять в каждом пункте назначения
     private Integer[][] amount;//Объем товаров после разгрузки
 
     private static final int WIDTH = 10;
 
-    public TableTransportTask(Integer[][] costs, Integer[] supplies, Integer[] demands) {
+    public TableTransportTask(int[][] costs, int[] supplies, int[] demands) {
         this.costs = costs;
         this.supplies = supplies;
         this.demands = demands;
@@ -27,7 +27,7 @@ public class TableTransportTask {
 
         if (sumSup.get() > sumDem.get()) {
             var newCosts = Arrays.copyOf(costs, costs.length + 1);
-            newCosts[costs.length] = new Integer[5];
+            newCosts[costs.length] = new int[5];
             costs = newCosts;
             demands = Arrays.copyOf(demands, demands.length + 1);
             demands[demands.length - 1] = sumSup.get() - sumDem.get();
@@ -35,7 +35,7 @@ public class TableTransportTask {
 
     }
 
-    public TableTransportTask northWestAngle() {
+    public void northWestAngle() {
         Integer[][] amount = new Integer[demands.length][supplies.length];
         var dem = Arrays.copyOf(demands, demands.length);
         var sup = Arrays.copyOf(supplies, supplies.length);
@@ -57,34 +57,71 @@ public class TableTransportTask {
             }
         }
         this.amount = amount;
-        return new TableTransportTask(amount, this.supplies, this.demands);
     }
 
 
+    public void printCosts(){
+      StringBuilder table = new StringBuilder();
+
+      table.append(String.format("%-" + WIDTH + "s", ""));
+      for (int i = 0; i < costs[0].length; i++) {
+        table.append(String.format("%-" + WIDTH + "d", supplies[i]));
+      }
+      table.append("\n");
+
+      for (int i = 0; i < costs.length; i++) {
+        table.append(String.format("%-" + WIDTH + "d", demands[i]));
+        for (int j = 0; j < costs[0].length; j++) {
+          table.append(String.format("%-" + WIDTH + "d", costs[i][j]));
+        }
+        table.append("\n");
+      }
+
+      table.append("\n");
+      System.out.println(table);
+    }
+
+  public void printAmount(){
+    StringBuilder table = new StringBuilder();
+
+    table.append(String.format("%-" + WIDTH + "s", ""));
+    for (int i = 0; i < amount[0].length; i++) {
+      table.append(String.format("%-" + WIDTH + "d", supplies[i]));
+    }
+    table.append("\n");
+
+    for (int i = 0; i < amount.length; i++) {
+      table.append(String.format("%-" + WIDTH + "d", demands[i]));
+      for (int j = 0; j < amount[0].length; j++) {
+        if (amount[i][j] != null) table.append(String.format("%-" + WIDTH + "d", amount[i][j]));
+        else {
+          table.append(String.format("%-" + WIDTH + "s", "---"));
+        }
+      }
+      table.append("\n");
+    }
+
+    table.append("\n");
+    System.out.println(table);
+  }
     @Override
     public String toString() {
         StringBuilder table = new StringBuilder();
 
-        // Header row
         table.append(String.format("%-" + WIDTH + "s", ""));
         for (int i = 0; i < costs[0].length; i++) {
             table.append(String.format("%-" + WIDTH + "d", supplies[i]));
         }
         table.append("\n");
 
-        // First table
         for (int i = 0; i < costs.length; i++) {
             table.append(String.format("%-" + WIDTH + "d", demands[i]));
             for (int j = 0; j < costs[0].length; j++) {
-                if (costs[i][j] != null) table.append(String.format("%-" + WIDTH + "d", costs[i][j]));
-                else {
-                    table.append(String.format("%-" + WIDTH + "s", "---"));
-                }
+
             }
             table.append("\n");
         }
 
-        // Empty line
         table.append("\n");
 
 
