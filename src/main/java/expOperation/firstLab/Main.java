7a1b6f9f333f67ae2c7da0f6b4152b5ec43b665b
morrs1 <grishkovea@gmail.com>
2024-04-03 18:25:10 +0300

    добавил выбор варианта

--- src/main/java/expOperation/firstLab/Main.java
+++ src/main/java/expOperation/firstLab/Main.java
@@ -5,21 +5,8 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-//    int[][] costs = {
-//        {1, 2, 3, 5, 2},
-//        {4, 6, 7, 3, 1},
-//        {2, 2, 3, 4, 5}
-//    };
-//    int[] supplies = {120, 120, 200, 180, 110};
-//    int[] demands = {200, 150, 350};
-    int[][] costs = {
-        {1, 4, 5, 3, 1},
-        {2, 1, 2, 1, 2},
-        {3, 1, 4, 2, 1}
-    };
-    int[] supplies = {100, 120, 130, 100, 90};
-    int[] demands = {300, 120, 300};
-    TableTransportTask mainTable = new TableTransportTask(costs, supplies, demands);
+    /////1 вариант/////
+    var mainTable = getTableTransportTask(9);
     System.out.println("Изначальная таблица:");
     mainTable.printTable(true);
     mainTable.checkForEquality();
@@ -30,6 +17,38 @@ public class Main {
     mainTable.printTable(false);
 
 
+  }
+
+
+
+
+  private static TableTransportTask getTableTransportTask(int Task) {
+    int[][] costs = new int[0][0];
+    int[] supplies = new int[0];
+    int[] demands = new int[0];
+
+    switch (Task){
+      case 1->{
+        costs = new int[][]{
+            {1, 2, 3, 5, 2},
+            {4, 6, 7, 3, 1},
+            {2, 2, 3, 4, 5}
+        };
+        supplies = new int[]{120, 120, 200, 180, 110};
+        demands = new int[]{200, 150, 350};
+      }
+      case 9->{
+        costs = new int[][]{
+            {1, 4, 5, 3, 1},
+            {2, 1, 2, 1, 2},
+            {3, 1, 4, 2, 1}
+        };
+    supplies = new int[]{100, 120, 130, 100, 90};
+    demands = new int[]{300, 120, 300};
+      }
+    }
+
+    return new TableTransportTask(costs, supplies, demands);
   }
 
 
