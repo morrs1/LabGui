f0a3d65c83c815d130516fa5f30747ee4515b3eb
morrs1 <grishkovea@gmail.com>
2024-04-03 18:12:20 +0300

    добавил нормальную проверка на закрытую задачу

--- src/main/java/expOperation/firstLab/Main.java
+++ src/main/java/expOperation/firstLab/Main.java
@@ -5,19 +5,29 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
+//    int[][] costs = {
+//        {1, 2, 3, 5, 2},
+//        {4, 6, 7, 3, 1},
+//        {2, 2, 3, 4, 5}
+//    };
+//    int[] supplies = {120, 120, 200, 180, 110};
+//    int[] demands = {200, 150, 350};
     int[][] costs = {
-        {1, 2, 3, 5, 2},
-        {4, 6, 7, 3, 1},
-        {2, 2, 3, 4, 5}
+        {1, 4, 5, 3, 1},
+        {2, 1, 2, 1, 2},
+        {3, 1, 4, 2, 1}
     };
-    int[] supplies = {120, 120, 200, 180, 110};
-    int[] demands = {200, 150, 350};
+    int[] supplies = {100, 120, 130, 100, 90};
+    int[] demands = {300, 120, 300};
     TableTransportTask mainTable = new TableTransportTask(costs, supplies, demands);
-    mainTable.printCosts();
+    System.out.println("Изначальная таблица:");
+    mainTable.printTable(true);
     mainTable.checkForEquality();
     mainTable.northWestAngle();
-    mainTable.printCosts();
-    mainTable.printAmount();
+    System.out.println("Таблица после проверки на закрытую/незакрытую:");
+    mainTable.printTable(true);
+    System.out.println("Таблица после метода северо-западного угла:");
+    mainTable.printTable(false);
 
 
   }
--- src/main/java/expOperation/firstLab/TableTransportTask.java
+++ src/main/java/expOperation/firstLab/TableTransportTask.java
@@ -6,8 +6,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TableTransportTask {
 
     private int[][] costs; // Затраты на перевоз товаров
-    private int[] demands; // Объемы товаров, которые нужно перевезти из каждого пункта отправления
-    private int[] supplies; // Объемы товаров, которые нужно принять в каждом пункте назначения
+    private int[] demands; // Заголовки слева и их объем
+    private int[] supplies; // Заголовки сверху и их объем
     private Integer[][] amount;//Объем товаров после разгрузки
 
     private static final int WIDTH = 10;
@@ -21,17 +21,24 @@ public class TableTransportTask {
     public void checkForEquality() {
         AtomicInteger sumSup = new AtomicInteger();
         Arrays.stream(supplies).forEach(sumSup::addAndGet);
-
         AtomicInteger sumDem = new AtomicInteger();
         Arrays.stream(demands).forEach(sumDem::addAndGet);
 
         if (sumSup.get() > sumDem.get()) {
             var newCosts = Arrays.copyOf(costs, costs.length + 1);
-            newCosts[costs.length] = new int[5];
+            newCosts[costs.length] = new int[supplies.length];
             costs = newCosts;
             demands = Arrays.copyOf(demands, demands.length + 1);
             demands[demands.length - 1] = sumSup.get() - sumDem.get();
         }
+      if (sumSup.get() < sumDem.get()) {
+        supplies= Arrays.copyOf(supplies, supplies.length+1);
+        supplies[supplies.length - 1] = sumDem.get() - sumSup.get();
+        for(var i= 0; i < demands.length; i++){
+          costs[i] = Arrays.copyOf(costs[i], supplies.length);
+        }
+        }
+//      }
 
     }
 
@@ -60,42 +67,28 @@ public class TableTransportTask {
     }
 
 
-    public void printCosts(){
-      StringBuilder table = new StringBuilder();
-
-      table.append(String.format("%-" + WIDTH + "s", ""));
-      for (int i = 0; i < costs[0].length; i++) {
-        table.append(String.format("%-" + WIDTH + "d", supplies[i]));
-      }
-      table.append("\n");
-
-      for (int i = 0; i < costs.length; i++) {
-        table.append(String.format("%-" + WIDTH + "d", demands[i]));
-        for (int j = 0; j < costs[0].length; j++) {
-          table.append(String.format("%-" + WIDTH + "d", costs[i][j]));
-        }
-        table.append("\n");
-      }
-
-      table.append("\n");
-      System.out.println(table);
-    }
-
-  public void printAmount(){
+  public void printTable(boolean printCosts) {
     StringBuilder table = new StringBuilder();
 
+    // Вывод заголовка
     table.append(String.format("%-" + WIDTH + "s", ""));
-    for (int i = 0; i < amount[0].length; i++) {
+    for (int i = 0; i < (printCosts ? costs[0].length : amount[0].length); i++) {
       table.append(String.format("%-" + WIDTH + "d", supplies[i]));
     }
     table.append("\n");
 
-    for (int i = 0; i < amount.length; i++) {
+    // Вывод основного содержимого
+    for (int i = 0; i < (printCosts ? costs.length : amount.length); i++) {
       table.append(String.format("%-" + WIDTH + "d", demands[i]));
-      for (int j = 0; j < amount[0].length; j++) {
-        if (amount[i][j] != null) table.append(String.format("%-" + WIDTH + "d", amount[i][j]));
-        else {
-          table.append(String.format("%-" + WIDTH + "s", "---"));
+      for (int j = 0; j < (printCosts ? costs[0].length : amount[0].length); j++) {
+        if (printCosts) {
+          table.append(String.format("%-" + WIDTH + "d", costs[i][j]));
+        } else {
+          if (amount[i][j] != null) {
+            table.append(String.format("%-" + WIDTH + "d", amount[i][j]));
+          } else {
+            table.append(String.format("%-" + WIDTH + "s", "---"));
+          }
         }
       }
       table.append("\n");
@@ -104,28 +97,5 @@ public class TableTransportTask {
     table.append("\n");
     System.out.println(table);
   }
-    @Override
-    public String toString() {
-        StringBuilder table = new StringBuilder();
-
-        table.append(String.format("%-" + WIDTH + "s", ""));
-        for (int i = 0; i < costs[0].length; i++) {
-            table.append(String.format("%-" + WIDTH + "d", supplies[i]));
-        }
-        table.append("\n");
-
-        for (int i = 0; i < costs.length; i++) {
-            table.append(String.format("%-" + WIDTH + "d", demands[i]));
-            for (int j = 0; j < costs[0].length; j++) {
-
-            }
-            table.append("\n");
-        }
-
-        table.append("\n");
-
-
-        return table.toString();
-    }
 
 }
