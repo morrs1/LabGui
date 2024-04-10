c0cc4a28aa48904743bbe195ce15e4f563f85bf2
morrs1 <grishkovea@gmail.com>
2024-04-10 19:40:43 +0300

    Первое задание 5 лабы по жаве

--- src/main/java/Programming/FourthSemLab/FifthLab/Main.java
+++ src/main/java/Programming/FourthSemLab/FifthLab/Main.java
@@ -1,13 +1,25 @@
 package Programming.FourthSemLab.FifthLab;
 
+import java.util.Arrays;
+
 public class Main {
 
   public static void main(String[] args) {
 
   }
 
-  public String firstTask(String args){
-    return "";
+  public String firstTask(String ignoredUnused){
+    var res = new StringBuilder();
+    Pair<String, Integer> pair = Pair.makePair("foo", 42);
+    res.append(pair.getFirst()).append(" ");
+    res.append(pair.getSecond()).append("\n");
+
+    String[] strings = {"hello", "world"};
+    Pair<String, String[]> pair2 = Pair.makePair("foo", strings);
+    res.append(pair2.getFirst()).append(" ");
+    res.append(Arrays.toString(pair2.getSecond()));
+    return res.toString();
+
   }
 
   public void secondTask(String args){
---
+++ src/main/java/Programming/FourthSemLab/FifthLab/Pair.java
@@ -0,0 +1,18 @@
+package Programming.FourthSemLab.FifthLab;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Pair<T extends Object, K extends Object> {
+
+  private T first;
+  private K second;
+
+  static <T, K> Pair<T, K> makePair(T firstElem, K secondElem) {
+    return new Pair<>(firstElem, secondElem);
+  }
+}
