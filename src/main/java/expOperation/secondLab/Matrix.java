862ce3248f5422a88cbebe32c0eff1b69a3be9a9
morrs1 <grishkovea@gmail.com>
2024-04-18 19:40:07 +0300

    сделал умножение матриц, но пока не по очереди

--- src/main/java/expOperation/secondLab/Matrix.java
+++ src/main/java/expOperation/secondLab/Matrix.java
@@ -8,110 +8,113 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Matrix {
-    static StringBuilder res = new StringBuilder();
 
-    public static void printMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.printf("%10d ", matrix[i][j]);
-            }
-            System.out.println("\n\n");
-        }
+  static StringBuilder res = new StringBuilder();
+  static int countOperations = 0;
+
+  public static void printMatrix(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        System.out.printf("%10d ", matrix[i][j]);
+      }
+      System.out.println("\n\n");
+    }
+  }
+
+  public static int[][] matrixMultiply(int[][] A, int[][] B) {
+    int rowsA = A.length;
+    int columnsA = A[0].length;
+    int rowsB = B.length;
+    int columnsB = B[0].length;
+
+    if (columnsA != rowsB) {
+      throw new IllegalArgumentException("Mismatch in matrix dimensions");
     }
 
-    public static int[][] matrixMultiply(int[][] A, int[][] B) {
-        int rowsA = A.length;
-        int columnsA = A[0].length;
-        int rowsB = B.length;
-        int columnsB = B[0].length;
+    int[][] C = new int[rowsA][columnsB];
 
-        if (columnsA != rowsB) {
-            throw new IllegalArgumentException("Mismatch in matrix dimensions");
+    for (int i = 0; i < rowsA; i++) {
+      for (int j = 0; j < columnsB; j++) {
+        for (int k = 0; k < columnsA; k++) {
+          C[i][j] += A[i][k] * B[k][j];
+          countOperations+=1;
         }
-
-        int[][] C = new int[rowsA][columnsB];
-
-        for (int i = 0; i < rowsA; i++) {
-            for (int j = 0; j < columnsB; j++) {
-                for (int k = 0; k < columnsA; k++) {
-                    C[i][j] += A[i][k] * B[k][j];
-                }
-            }
-        }
-
-        return C;
+      }
     }
 
-    public static void matrixChainOrder(int[] p, int[][] m, int[][] s) {
-        int n = p.length - 1;
+    return C;
+  }
 
-        // Initialize matrix m
-        for (int i = 1; i <= n; i++) {
-            m[i][i] = 0;
-        }
+  public static void matrixChainOrder(int[] p, int[][] m, int[][] s) {
+    int n = p.length - 1;
 
-        // Compute m[i, j] for all i, j
-        for (int l = 2; l <= n; l++) {
-            for (int i = 1; i <= n - l + 1; i++) {
-                int j = i + l - 1;
-                m[i][j] = Integer.MAX_VALUE;
-                for (int k = i; k <= j - 1; k++) {
-                    int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-                    if (q < m[i][j]) {
-                        m[i][j] = q;
-                        s[i][j] = k;
-                    }
-                }
-            }
-        }
+    // Initialize matrix m
+    for (int i = 1; i <= n; i++) {
+      m[i][i] = 0;
     }
 
-    public static void printOptimalParens(int[][] s, int i, int j) {
-        if (i == j) {
-            res.append("A").append(i);
-        } else {
-            res.append("(");
-            printOptimalParens(s, i, s[i][j]);
-            printOptimalParens(s, s[i][j] + 1, j);
-            res.append(")");
+    // Compute m[i, j] for all i, j
+    for (int l = 2; l <= n; l++) {
+      for (int i = 1; i <= n - l + 1; i++) {
+        int j = i + l - 1;
+        m[i][j] = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+          int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+          if (q < m[i][j]) {
+            m[i][j] = q;
+            s[i][j] = k;
+          }
         }
+      }
     }
+  }
 
-    public static LinkedHashMap<String, String> getOrderMultiply() {
-        String inputString = res.toString();
+  public static void printOptimalParens(int[][] s, int i, int j) {
+    if (i == j) {
+      res.append("A").append(i);
+    } else {
+      res.append("(");
+      printOptimalParens(s, i, s[i][j]);
+      printOptimalParens(s, s[i][j] + 1, j);
+      res.append(")");
+    }
+  }
 
-        // Регулярное выражение для поиска матриц
-        String patternString = "\\b([A-Za-z0-9]+)\\b";
+  public static ArrayList<String> getOrderMultiply(int[][][] arrA) {
+    String inputString = res.toString();
 
-        // Создание объекта Pattern
-        Pattern pattern = Pattern.compile(patternString);
+    // Регулярное выражение для поиска матриц
+    String patternString = "\\b([A-Za-z0-9]+)\\b";
 
-        // Создание объекта Matcher для исходной строки
-        Matcher matcher = pattern.matcher(inputString);
+    // Создание объекта Pattern
+    Pattern pattern = Pattern.compile(patternString);
 
-        // Список для хранения найденных матриц
-        List<String> foundMatrices = new ArrayList<>();
+    // Создание объекта Matcher для исходной строки
+    Matcher matcher = pattern.matcher(inputString);
 
-        var c = 7;
-        while(res.length()>3){
-            matcher = pattern.matcher(res);
-            while (matcher.find()) {
-                // Извлечение текущего совпадения и добавление его в список
-                if (matcher.group().length() > 3) {
-                    foundMatrices.add(matcher.group());
-                }
-            }
-            for (var l : foundMatrices) {
-                if(res.indexOf("(" + l + ")") != -1){
-                    res = new StringBuilder(res.toString().replace("(" + l + ")", "A"+c));
-                    c+=1;
-                }
+    // Список для хранения найденных матриц
+    ArrayList<String> foundMatrices = new ArrayList<>();
 
-            }
-
-            System.out.println(foundMatrices);
-            System.out.println(res.toString());
+    var c = arrA.length + 1;
+    while (res.length() > 3) {
+      matcher = pattern.matcher(res);
+      while (matcher.find()) {
+        // Извлечение текущего совпадения и добавление его в список
+        if (matcher.group().length() > 3) {
+          foundMatrices.add(matcher.group());
         }
+      }
+      for (var l : foundMatrices) {
+        if (res.indexOf("(" + l + ")") != -1) {
+          res = new StringBuilder(res.toString().replace("(" + l + ")", "A" + c));
+          c += 1;
+        }
+
+      }
+
+      System.out.println(foundMatrices);
+      System.out.println(res.toString());
+    }
 //        // Цикл по всем совпадениям
 //        while (matcher.find()) {
 //            // Извлечение текущего совпадения и добавление его в список
@@ -168,27 +171,81 @@ public class Matrix {
 //
 //        System.out.println(foundMatrices);
 //        System.out.println(res.toString());
-        return null;
+    return foundMatrices;
+  }
+
+  public static void multiplyOrderedMatrix(int[][][] arrA, ArrayList<String> order) {
+var A7 = matrixMultiply(arrA[0], arrA[1]);
+    System.out.println(Arrays.deepToString(A7) + "\n");
+var A8 = matrixMultiply(arrA[2], arrA[3]);
+    System.out.println(Arrays.deepToString(A8) + "\n");
+var A9 = matrixMultiply(arrA[4], arrA[5]);
+    System.out.println(Arrays.deepToString(A9) + "\n");
+var A10 = matrixMultiply(A8, A9);
+    System.out.println(Arrays.deepToString(A10) + "\n");
+var A11 = matrixMultiply(A7, A10);
+    for(var a:A11){
+      System.out.println(Arrays.toString(a));
     }
+    System.out.println(countOperations);
+  }
 
-    public static void main(String[] args) {
-        int[] p = {30, 35, 15, 5, 10, 20, 25};
-        int n = p.length;
+  public static void main(String[] args) {
+    int[][] A1 =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+    int[][] A2 =
+        {
+            {1},
+            {4},
+            {7}
+        };
+    int[][] A3 =
+        {
+            {1, 2, 3, 5, 6}
+        };
+    int[][] A4 =
+        {
+            {1},
+            {4},
+            {7},
+            {8},
+            {9}
+        };
+    int[][] A5 =
+        {
+            {1, 2}
+        };
 
-        int[][] m = new int[n][n];
-        int[][] s = new int[n][n];
+    int[][] A6 =
+        {
+            {1, 2},
+            {4, 5}
+        };
+    int[][][] arrA = {A1, A2, A3, A4, A5, A6};
+//    int[] p = {30, 30, 10, 50, 10, 20, 20};
+    int[] p = {3, 3, 1, 5, 1, 2, 2};
+    int n = p.length;
 
-        matrixChainOrder(p, m, s);
+    int[][] m = new int[n][n];
+    int[][] s = new int[n][n];
 
-        System.out.println("Minimum number of multiplications: " + m[1][n - 1]);
-        System.out.print("Optimal parenthesization: ");
+    matrixChainOrder(p, m, s);
 
-        printOptimalParens(s, 1, n - 1);
-        System.out.println(res.toString());
-        System.out.println();
-        System.out.println(Arrays.deepToString(s));
-        System.out.println(Arrays.deepToString(m));
-        getOrderMultiply();
-    }
+    System.out.println("Minimum number of multiplications: " + m[1][n - 1]);
+    System.out.print("Optimal parenthesization: ");
+
+    printOptimalParens(s, 1, n - 1);
+    System.out.println(res.toString());
+//    for(var ss: m){
+//      System.out.println(Arrays.toString(ss));
+//    }
+//    System.out.println(Arrays.deepToString(s));
+    System.out.println();
+    multiplyOrderedMatrix(arrA, getOrderMultiply(arrA));
+  }
 }
 
